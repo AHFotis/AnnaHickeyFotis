2d683d9ca3f4a68db72a2f72d6b2ac8d26f15235
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-05-02 15:05:31 -0500

    Skills section styled

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -21,70 +21,83 @@ function Skills() {
 
 			<section id="skills">
 				<div className="container">
-				<h2>Skilled In</h2>
-				<div className="row">
+				<h2>Anna Is Skilled In</h2>
+				<hr></hr>
+				<div className="row rowOne">
 					<div className="col-sm-2">
-						<p>HTML5</p>
+						
 						<img className="img img-thumbnail img-fluid icon" src={html}
 										alt="html"></img>
+										<p>HTML5</p>
 					</div>
 					<div className="col-sm-2">
-					<p>CSS</p>
+					
 						<img className="img img-thumbnail img-fluid icon" src={css}
 										alt="css"></img>
+										<p>CSS</p>
 					</div>
 					<div className="col-sm-2">
-					<p>JavaScript and ES6</p>
+					
 						<img className="img img-thumbnail img-fluid icon" src={js}
 										alt="js"></img>
+										<p>JavaScript and ES6</p>
 					</div>
 					<div className="col-sm-2">
-					<p>AJAX/Axios</p>
+					
 						<img className="img img-thumbnail img-fluid icon" src={ajax}
 										alt="ajax"></img>
+										<p>AJAX/Axios</p>
 					</div>
 					<div className="col-sm-2">
-					<p>jQuery</p>
+					
 						<img className="img img-thumbnail img-fluid icon" src={jquery}
 										alt="jquery"></img>
+										<p>jQuery</p>
 						
 					</div>
 					<div className="col-sm-2">
-					<p>Git</p>
+					
 						<img className="img img-thumbnail img-fluid icon" src={git}
 										alt="git"></img>
+										<p>Git</p>
 					</div>
 				</div>
-				<div className="row">
+				<div className="row rowTwo">
 					<div className="col-sm-2">
-					<p>Node.js</p>
+					
 						<img className="img img-thumbnail img-fluid icon" src={node}
 										alt="node"></img>
+										<p>Node.js</p>
 					</div>
 					<div className="col-sm-2">
-					<p>Express</p>
+					
 						<img className="img img-thumbnail img-fluid icon" src={express}
 										alt="express"></img>
+										<p>Express</p>
 					</div>
 					<div className="col-sm-2">
-					<p>Visual Studio Code</p>
+					
 						<img className="img img-thumbnail img-fluid icon" src={vscode}
 										alt="vscode"></img>
+										<p>Visual Studio Code</p>
 					</div>
 					<div className="col-sm-2">
-					<p>MongoDB</p>
+					
 						<img className="img img-thumbnail img-fluid icon" src={mongodb}
 										alt="mongodb"></img>
+										<p>MongoDB</p>
 					</div>
 					<div className="col-sm-2">
-					<p>MySQL</p>
+					
 						<img className="img img-thumbnail img-fluid icon" src={mysql}
 										alt="mysql"></img>
+										<p>MySQL</p>
 					</div>
 					<div className="col-sm-2">
-					<p>React</p>
+					
 						<img className="img img-thumbnail img-fluid icon" src={react}
 										alt="react"></img>
+										<p>React</p>
 					</div>
 				</div>
 				</div>
