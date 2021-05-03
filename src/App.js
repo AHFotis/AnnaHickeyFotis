8503223e9df3a4f7f4c6d01a3a92d52c61d8258f
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-05-03 10:44:40 -0500

    Portfolio Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import React from "react";
 import Home from "./pages/Home";
-// import Portfolio from "./pages/Portfolio";
+import Portfolio from "./pages/Portfolio";
 import { HashRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
          <Route exact path="/" component={Home}/>
+         <Route exact path="/portfolio" component={Portfolio}/>
     </Router>
      
   );
---
+++ src/pages/Portfolio/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Footer from "../../components/Footer/index"
+import Header from "../../components/Header"
+import "./styles.css"
+
+
+
+
+function Portfolio() {
+    return (
+
+        <div>
+
+            <Header />
+
+
+            <main className="mainBody">
+
+                <header id="portfolio">
+
+                    <div className="pTitle">
+                        <div className="row">
+                            
+                                <h1 className="pHeader">Portfolio</h1>
+                             
+                        </div>
+                    </div>
+                    
+
+
+                </header>
+
+
+               
+
+            </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default Portfolio;
